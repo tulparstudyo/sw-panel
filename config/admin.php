7f1f4244a9391a848ff105179c98184e1f93f6ec
tulparstudyo <teknovipyazilim@gmail.com>
2021-07-20 09:17:17 +0300

    fixed

--- config/admin.php
+++ config/admin.php
@@ -1,32 +1,8 @@
 <?php
 
 return [
-    'jqadm' => [
-        'navbar' => [
-            'swordbros'=>[
-                ''=>'swordbros',
-                '10'=>'swordbros/frigian',
-                '20'=>'swordbros/slider'
-            ]
-        ],
-        'resource' =>[
-            'swordbros' => [
-                'groups' => ['admin', 'editor', 'super'],
-                'frigian' =>[
-                    'groups' => ['admin', 'editor', 'super'],
-                    'key' => 'SF',
-                ],
-                'slider' =>[
-                    'groups' => ['admin', 'editor', 'super'],
-                    'key' => 'SS',
-                ],
-                /*'blog' =>[
-                    'groups' => ['admin', 'editor', 'super'],
-                    'key' => 'SB',
-                ],*/
-            ],
-        ],
-    ],
-    'jsonadm' => [
-    ],
+	'jqadm' => [
+	],
+	'jsonadm' => [
+	],
 ];
