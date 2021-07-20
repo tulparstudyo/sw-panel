3b35f8dec60040a6390379ce2bb108c1e716dc7e
tulparstudyo <teknovipyazilim@gmail.com>
2021-07-20 10:43:14 +0300

    main menu

--- config/admin.php
+++ config/admin.php
@@ -1,8 +1,32 @@
 <?php
 
 return [
-	'jqadm' => [
-	],
-	'jsonadm' => [
-	],
+    'jqadm' => [
+        'navbar' => [
+            'swordbros'=>[
+                ''=>'swordbros',
+                '10'=>'swordbros/theme',
+                '20'=>'swordbros/slider'
+            ]
+        ],
+        'resource' =>[
+            'swordbros' => [
+                'groups' => ['admin', 'editor', 'super'],
+                'frigian' =>[
+                    'groups' => ['admin', 'editor', 'super'],
+                    'key' => 'SF',
+                ],
+                'slider' =>[
+                    'groups' => ['admin', 'editor', 'super'],
+                    'key' => 'SS',
+                ],
+                /*'blog' =>[
+                    'groups' => ['admin', 'editor', 'super'],
+                    'key' => 'SB',
+                ],*/
+            ],
+        ],
+    ],
+    'jsonadm' => [
+    ],
 ];
